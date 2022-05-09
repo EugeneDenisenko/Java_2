51c23ed52b45bcc8b7cb50a64db2167cebbd9a95
Евгений <evg.denisenko@gmail.com>
2022-05-09 22:49:19 +0300

    lesson 6 repeat

--- ClientChat/src/main/java/com/geekbrains/ckientchat/ClientChat.java
+++ ClientChat/src/main/java/com/geekbrains/ckientchat/ClientChat.java
@@ -1,24 +1,68 @@
 package com.geekbrains.ckientchat;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import static com.geekbrains.ckientchat.Network.SERVER_HOST;
+import static com.geekbrains.ckientchat.Network.SERVER_PORT;
 
 public class ClientChat extends Application {
+
+    private Stage stage;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ClientChat.class.getResource("chat-template.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Student project");
-        stage.setScene(scene);
+        this.stage = stage;
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(ClientChat.class.getResource("chat-template.fxml"));
+
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        this.stage.setTitle("Student project");
+        this.stage.setScene(scene);
 
         ClientController controller = fxmlLoader.getController();
         controller.userList.getItems().addAll("user1", "user2");
 
         stage.show();
+        connectToServer(controller);
+    }
+
+    private void connectToServer(ClientController clientController) {
+        Network network = new Network();
+        boolean resultConnectedToServer = network.connect();
+        if (!resultConnectedToServer) {
+            String errorMessage = "Невозможно установить сетевое соединение.";
+            System.err.println(errorMessage);
+            showErrorDialog(errorMessage);
+        }
+        clientController.setNetwork(network);
+        clientController.setApplication(this);
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent windowEvent) {
+                network.close();
+            }
+        });
+    }
+
+    public void showErrorDialog(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Ошибка");
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 
     public static void main(String[] args) {
